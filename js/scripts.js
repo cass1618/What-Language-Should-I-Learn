d8058c9fba4a70e14fc348b6a9269808e5517ae2
cass1618 <cass16180@gmail.com>
2021-03-26 14:08:36 -0700

    Complete decision function

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 // BUSINESS LOGIC
-function decision (total) {
+function decision () {
+  let total = 0
+  for (i = 0; i < arguments.length; i++)
+  {
+    total += arguments[i];
+  }
   if (total < 10) {
     return "Ruby";
   } else if (total >= 10 && total < 20) {
     return "Javascript";
-  } else if (total >= 10 && total < 20) {
+  } else if (total >= 20 && total < 30) {
     return "Swift";
-  } else if (total >= 10 && total < 20) {
+  } else if (total >= 30 && total < 40) {
     return "Java";
-  } else if (total >= 10 && total < 20) {
+  } else if (total >= 40 && total < 50) {
     return "Python";
   } else {
     return "error";
   }
-}
-
-function add (...nums) {
-  let sum = nums.reduce((num1, num2) => num1 + num2);
-  return sum;
-}
+  }
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
@@ -26,14 +26,9 @@ $(document).ready(function() {
     const a = parseInt($("input:radio[name=fieldRadio]:checked").val());
     const b = parseInt($("input:radio[name=cityRadio]:checked").val());
     const c = parseInt($("input:radio[name=cityRadio]:checked").val());
-    event.preventDefault();
     
-    
-    let sum = a + b + c;
-    console.log(sum);
-    
-    $(".result").text(decision(sum));
+    event.preventDefault();     
+    $(".result").text(decision(a,b,c));
     $(".hidden").show();
-    
   });
 });
