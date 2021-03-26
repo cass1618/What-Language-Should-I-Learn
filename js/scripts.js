55eaab2b32faa403b0d03c4aa0e7d93e4695a613
cass1618 <cass16180@gmail.com>
2021-03-26 08:56:24 -0700

    Assign inputs to constants

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// BUSINESS LOGIC
+
+
+// USER INTERFACE LOGIC
+$(document).ready(function() {
+  $("form#mainForm").submit(function(event) {
+    const a = $("#blankA").val();
+    const b = $("#blankA").val();
+    const c = $("#blankA").val();
+    const d = $("#blankA").val();
+    const e = $("#blankA").val();
+    const f = $("#blankA").val();
+    
+
+  });
+});
