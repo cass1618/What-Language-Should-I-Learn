6c3fa4bb40438efd82d10cdd94be4cc2a40cf38d
cass1618 <cass16180@gmail.com>
2021-03-26 11:47:35 -0700

    Add function to compute score and return decision

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,33 @@
 // BUSINESS LOGIC
-
+function decision (total) {
+  if (total < 10) {
+    return "Ruby";
+  } else if (total >= 10 && total < 20) {
+    return "Javascript";
+  } else if (total >= 10 && total < 20) {
+    return "Swift";
+  } else if (total >= 10 && total < 20) {
+    return "Java";
+  } else if (total >= 10 && total < 20) {
+    return "Python";
+  } else {
+    return "error";
+  }
+}
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
   $("form#mainForm").submit(function(event) {
     const a = $("input:radio[name=fieldRadio]:checked").val();
     const b = $("input:radio[name=cityRadio]:checked").val();
-    console.log("b is "+b)    
+    const c = $("input:radio[name=cityRadio]:checked").val();
 
     event.preventDefault();
 
+    let sum = a + b + c;
+    
+    $(".result").text(decision(sum));
+    $(".answer").show();
+    
   });
 });
